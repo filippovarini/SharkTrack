0a636d4f2dd83700251cb484e31c42ed79ec4613
Filippo Varini <filippo@alpha10x.com>
2023-12-07 12:04:54 +0000

    fix kalman filters iteration

--- sd_bruv_tracking_kalman.py
+++ sd_bruv_tracking_kalman.py
@@ -185,7 +185,7 @@ for vid in os.listdir(vid_dir): # iterate through each video in vid_dir
                 cv2.imwrite("live.jpg", retrieve_frame(cap, count, frame_cap)[1]) # view unaltered frames
             
             # Predict next location of Kalman Filters regardless of whether a shark was detected
-            for i in len(kalman_filters):
+            for i in range(len(kalman_filters)):
                 if i == kalman_index:
                     continue
                 kf = kf[i][0]
